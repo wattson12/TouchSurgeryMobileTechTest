1991ceb550e98eb85f83d9e0f5bc72779c59952a
Sam Watts <samuel.watts@gmail.com>
2018-04-13 14:28:23 +0100

    added cell subclass for procedure

---
+++ TouchSurgeryTechTest/Screens/Procedures List/ProcedureTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProcedureTableViewCell.swift
+//  TouchSurgeryTechTest
+//
+//  Created by Sam Watts on 13/04/2018.
+//  Copyright © 2018 Sam Watts. All rights reserved.
+//
+
+import UIKit
+
+class ProcedureTableViewCell: UITableViewCell {
+
+    
+}
