af424cc9cb02fcbd53d27a223e0a443d1be27b30
Sam Watts <samuel.watts@gmail.com>
2018-04-13 14:56:23 +0100

    connected view model to temporary table view data source

--- TouchSurgeryTechTest/Screens/Procedures List/ProcedureListTableViewController.swift
+++ TouchSurgeryTechTest/Screens/Procedures List/ProcedureListTableViewController.swift
@@ -10,4 +10,51 @@ import UIKit
 
 class ProcedureListTableViewController: BaseViewController {
 
+    @available(iOS, unavailable, message: "init() is unavailable, use init(viewModel:) instead")
+    override init() { fatalError() }
+
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .background
+        tableView.registerReusableElement(ProcedureTableViewCell.self)
+        tableView.tableFooterView = UIView() //empty footer to remote empty cell lines after content
+        return tableView
+    }()
+
+    let viewModel: ProcedureListViewModel
+
+    init(viewModel: ProcedureListViewModel) {
+        self.viewModel = viewModel
+
+        super.init()
+    }
+
+    override func loadView() {
+        self.view = tableView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+}
+
+extension ProcedureListTableViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.procedures.value.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ProcedureTableViewCell.reuseIdentifier, for: indexPath) as? ProcedureTableViewCell else {
+            fatalError("Unregistered cell used on table view")
+        }
+
+        let procedure = viewModel.procedures.value[indexPath.row]
+        cell.textLabel?.text = procedure.0
+
+        return cell
+    }
 }
