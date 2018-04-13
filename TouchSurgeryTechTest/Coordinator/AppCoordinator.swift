aa3680a236fb8e01396153858e53756c5edf9fe5
Sam Watts <samuel.watts@gmail.com>
2018-04-13 14:18:53 +0100

    Added app coordinator to handle app setup + navigation

---
+++ TouchSurgeryTechTest/Coordinator/AppCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  AppCoordinator.swift
+//  TouchSurgeryTechTest
+//
+//  Created by Sam Watts on 13/04/2018.
+//  Copyright © 2018 Sam Watts. All rights reserved.
+//
+
+import UIKit
+
+final class AppCoordinator {
+
+    private let window: UIWindow
+
+    init(window: UIWindow = UIWindow(frame: UIScreen.main.bounds)) {
+        self.window = window
+    }
+
+    func start() {
+        let procedureListViewController = UIViewController()
+        let rootNavigationController = UINavigationController(rootViewController: procedureListViewController)
+
+        window.rootViewController = rootNavigationController
+
+        window.makeKeyAndVisible()
+    }
+}
